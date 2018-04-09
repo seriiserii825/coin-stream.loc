d92ab8bed1adf367196abc0352dcc5bb603ea2c9
serii <seriiburduja@gmail.com>
2018-04-09 16:48:14 +0300

    finish clients

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -41,5 +41,39 @@ $(function () {
     }
   });
 
+  /*clients__slider
+  =======================================*/ 
+  $('#js-clients__list').slick({
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+  });
+
   
 });
